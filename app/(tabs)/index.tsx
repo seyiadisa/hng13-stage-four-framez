02fe3a316150764ae3898a968f93537aefd58685
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2025-11-13 03:57:09 +0100

    feat: add home feed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,40 @@
-import { PageContainer } from "@/components/container";
-import { BodyText, TitleText } from "@/components/typography";
+import { ScrollContainer } from "@/components/container";
+import FeedPost from "@/components/feed-post";
+import { TitleText } from "@/components/typography";
+import { useHomeFeed } from "@/hooks/use-feed";
 import { View } from "react-native";
 
 export default function Index() {
+  const { data: homeFeed } = useHomeFeed();
+
   return (
-    <PageContainer>
+    <ScrollContainer>
       <View style={{ alignItems: "flex-start", width: "100%" }}>
         <TitleText bold>Good Morning, Seyi</TitleText>
       </View>
 
-      <View>
-        <BodyText>TODO: My feed</BodyText>
-      </View>
-    </PageContainer>
+      {!homeFeed || homeFeed.length === 0 ? (
+        <View
+          style={{
+            width: "100%",
+            marginTop: 32,
+            alignItems: "center",
+          }}
+        >
+          <TitleText>
+            Your feed is empty. Follow users to see their posts here!
+          </TitleText>
+        </View>
+      ) : (
+        homeFeed?.map((post) => (
+          <FeedPost
+            key={post.id}
+            poster_id={post.author_id}
+            text={post.content}
+            imagePath={post.media_url}
+          />
+        ))
+      )}
+    </ScrollContainer>
   );
 }
--- hooks/use-feed.ts
+++ hooks/use-feed.ts
@@ -2,7 +2,36 @@ import { supabase } from "@/lib/supabase";
 import { useQuery } from "@tanstack/react-query";
 import { Alert } from "react-native";
 
-export const useHomeFeed = () => {};
+export const useHomeFeed = () => {
+  return useQuery({
+    queryKey: ["home_feed"],
+    refetchOnMount: "always",
+
+    queryFn: async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      const { data } = await supabase
+        .from("follows")
+        .select("following_id")
+        .eq("follower_id", user!.id);
+
+      const followingIds = data?.map((follow) => follow.following_id);
+      const { data: posts, error } = await supabase
+        .from("posts")
+        .select("*")
+        .in("author_id", followingIds || [])
+        .order("created_at", { ascending: false });
+
+      if (error) {
+        Alert.alert("Error fetching home feed", error.message);
+        return [];
+      }
+      return posts;
+    },
+  });
+};
 
 export const useExploreFeed = () => {
   return useQuery({
